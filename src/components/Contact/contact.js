d9c4b73d2dd5dfa2dd7925b602fed05523b4ca9a
Marina <marina.webdev@gmail.com>
2021-03-02 14:19:14 +0100

    - remove content

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -32,7 +32,7 @@ const Contact = () => {
           {contact.source}
         </a>
       ))}
-      <p className={styles.creds}>created by myself. photos by  <a href={'https://www.instagram.com/cocainym/'}>jim lafleur</a> </p>
+      <p className={styles.creds}>photos by  <a href={'https://www.instagram.com/cocainym/'}>jim lafleur</a> </p>
       </div>
   )
 };
